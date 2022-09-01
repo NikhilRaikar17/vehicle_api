6330504fe47e39b8320d20f4b78ffb3f2d144eb4
NikhilRaikar17 <nikhilraikar88@gmail.com>
2022-09-01 04:41:53 +0200

    Documentation: implemented swagger documentation for add_vehicle

--- application/__init__.py
+++ application/__init__.py
@@ -1,14 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_swagger_ui import get_swaggerui_blueprint
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("config.DevelopmentConfig")
+    SWAGGER_URL = '/api'
+    API_URL = '/static/swagger.json'
+    SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={
+            'app_name': "IONOS-API"
+        }
+    )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     with app.app_context():
+        app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
         from application.vehicle import vehicle_api_blueprint
         app.register_blueprint(vehicle_api_blueprint)
         return app
