6bd1d9114c746b9f95a210280fdfe226fbb47e5b
NikhilRaikar17 <nikhilraikar88@gmail.com>
2022-09-01 05:02:52 +0200

    routes: implemented the update functionality routes

--- application/vehicle/routes.py
+++ application/vehicle/routes.py
@@ -21,6 +21,7 @@ def status():
 
 @vehicle_api_blueprint.route('/add_vehicle', methods=['POST'])
 def add_vehicle():
+    """Adds a new vehicle"""
     try:
         brand = request.form.get('vehicle_brand')
         description = request.form.get('vehicle_description')
@@ -52,4 +53,39 @@ def add_vehicle():
                     "Error": True,
                     "message":"Vehicle could not be created!"
                     })
+
+@app.route('/update_vehicle', methods=['PUT'])
+def update_vehicle():
+    try:
+        vehicle_id = request.args.get('id')
+        brand = request.form.get('vehicle_brand')
+        description = request.form.get('vehicle_description')
+        year_of_manufacture = request.form.get('year_of_manufacture', type=int)
+        ready_to_drive = request.form.get('ready_to_drive', type=bool)
+
+        error,message = vehicle_service.update(db,vehicle_id,brand,description,year_of_manufacture,ready_to_drive)
+        if error:
+            raise Exception(message)
+
+        return jsonify({
+                        "status": 200,
+                        "Error": False,
+                        "message":"vehicle successfully updated!"
+                        })
+
+    except Exception as e:
+        db.session.rollback()
+        db.session.flush()
+        message = e.args[0]
+        if message:
+            return jsonify({
+                    "status": 500,
+                    "Error": True,
+                    "message":message
+                    })
+        return jsonify({
+                "status": 500,
+                "Error": True,
+                "message":"Vehicle could not be updated!"
+                })
 
