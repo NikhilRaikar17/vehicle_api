1cb43ce77c1d0277dfe22976dafaff77d8d495be
NikhilRaikar17 <nikhilraikar88@gmail.com>
2022-09-01 03:48:22 +0200

    created entities for vehicle module

---
+++ application/vehicle/entities/vehicle_entities.py
@@ -0,0 +1,7 @@
+class ValidVehicle:
+
+    def __init__(self,brand,description,year_of_manufacture,ready_to_drive):
+        self.brand = brand
+        self.description = description
+        self.year_of_manufacture = year_of_manufacture
+        self.ready_to_drive = ready_to_drive
