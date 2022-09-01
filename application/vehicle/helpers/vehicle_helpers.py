51475e3d9d7c6dff75acb37b2f472819cf5defdc
NikhilRaikar17 <nikhilraikar88@gmail.com>
2022-09-01 04:18:54 +0200

    Helpers: helper functions created to assist add_vehicle service

--- application/vehicle/helpers/vehicle_helpers.py
+++ application/vehicle/helpers/vehicle_helpers.py
@@ -1,6 +1,7 @@
 from application.models import Vehicle
 
 def get_vehicle(brand,description,year_of_manufacture,ready_to_drive):
+    """Get a vehicle object from the database"""
     vehicle = Vehicle.query.filter_by(
                                     brand=brand,
                                     description=description,
@@ -13,6 +14,7 @@ def get_vehicle(brand,description,year_of_manufacture,ready_to_drive):
     return False
 
 def get_vehicle_brand(brand):
+    """Get a vehicle object from brand name"""
     vehicle = Vehicle.query.filter_by(
                                     brand=brand,
                                     ).first()
@@ -20,3 +22,17 @@ def get_vehicle_brand(brand):
         return True
 
     return False
+
+def create_vehicle(db,valid_vehicle):
+    """Create a new vehicle object"""
+    try:
+        new_vehicle = Vehicle(**vars(valid_vehicle))
+        db.session.add(new_vehicle)
+        db.session.commit()
+        return True
+    except Exception as e:
+        db.session.rollback()
+        db.session.flush()
+        return False
+
+
