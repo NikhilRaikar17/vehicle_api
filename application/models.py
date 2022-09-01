5be31d2d1f361d75e81397e82840fe311de10d95
NikhilRaikar17 <nikhilraikar88@gmail.com>
2022-09-01 05:31:19 +0200

    Model: Database models updated with vehicle name and unique constraints

--- application/models.py
+++ application/models.py
@@ -4,10 +4,13 @@ from application import db
 class Vehicle(db.Model):
     __tablename__ = 'vehicle'
     id = db.Column(db.Integer,primary_key=True)
-    brand = db.Column(db.String(), unique=True)
+    name = db.Column(db.String(),nullable=False)
+    brand = db.Column(db.String(),nullable=False)
     description = db.Column(db.String())
     year_of_manufacture = db.Column(db.Integer, nullable=False)
     ready_to_drive = db.Column(db.Boolean, nullable=False)
 
+    __table_args__ = (db.UniqueConstraint('name', 'brand','year_of_manufacture'),)
+
     def __repr__(self):
-        return '<brand {}>'.format(self.brand)
+        return '<name {}>'.format(self.name)
